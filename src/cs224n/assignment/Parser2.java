8afe06731bcba0569f5467bc244657595db38525
eltshan <wu.shen.eltshan@gmail.com>
2015-10-20 21:54:32 -0700

    test

--- src/cs224n/assignment/Parser2.java
+++ src/cs224n/assignment/Parser2.java
@@ -145,10 +145,12 @@ public class Parser2 implements Parser {
 							if (!right.containsKey(br.getRightChild()))
 								continue;
 							double prob = left.get(br.getLeftChild()) * right.get(br.getRightChild()) * br.getScore();
-							//if (!score.containsKey(br.getParent()) || scores.getCount(A, br.getParent()) < prob) {
-							if (scores.getCount(A, br.getParent()) < prob) {
+							// if (!score.containsKey(br.getParent()) ||
+							// scores.getCount(A, br.getParent()) < prob) {
+							if (scores.getCounter(A).containsKey(br.getParent())
+									|| scores.getCount(A, br.getParent()) < prob) {
 
-							// Filled the CKY table and track the split
+								// Filled the CKY table and track the split
 								// information
 								scores.setCount(A, br.getParent(), prob);
 
