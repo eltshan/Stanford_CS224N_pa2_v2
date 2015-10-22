f6fd86e50464a09b8cfb73c97a7865e242a14cb5
eltshan <wu.shen.eltshan@gmail.com>
2015-10-21 22:54:04 -0700

    a

--- src/cs224n/assignment/TreeAnnotations.java
+++ src/cs224n/assignment/TreeAnnotations.java
@@ -10,11 +10,11 @@ import cs224n.ling.Trees.MarkovizationAnnotationStripper;
 import cs224n.util.Filter;
 
 /**
- * Class which contains code for annotating and binarizing trees for
- * the parser's use, and debinarizing and unannotating them for
- * scoring.
+ * Class which contains code for annotating and binarizing trees for the
+ * parser's use, and debinarizing and unannotating them for scoring.
  */
 public class TreeAnnotations {
+	static int order = 2;
 
 	public static Tree<String> annotateTree(Tree<String> unAnnotatedTree) {
 
@@ -23,10 +23,11 @@ public class TreeAnnotations {
 		// TODO: change the annotation from a lossless binarization to a
 		// finite-order markov process (try at least 1st and 2nd order)
 
-		// TODO : mark nodes with the label of their parent nodes, giving a second
+		// TODO : mark nodes with the label of their parent nodes, giving a
+		// second
 		// order vertical markov process
-
-		return binarizeTree(unAnnotatedTree);
+		Tree<String> tmp = VerticalMarkovnize(unAnnotatedTree, "", false);
+		return binarizeTree(tmp);
 
 	}
 
@@ -35,51 +36,64 @@ public class TreeAnnotations {
 		if (tree.isLeaf())
 			return new Tree<String>(label);
 		if (tree.getChildren().size() == 1) {
-			return new Tree<String>
-			(label, 
-					Collections.singletonList(binarizeTree(tree.getChildren().get(0))));
+			return new Tree<String>(label, Collections.singletonList(binarizeTree(tree.getChildren().get(0))));
 		}
-		// otherwise, it's a binary-or-more local tree, 
+		// otherwise, it's a binary-or-more local tree,
 		// so decompose it into a sequence of binary and unary trees.
-		String intermediateLabel = "@"+label+"->";
-		Tree<String> intermediateTree =
-				binarizeTreeHelper(tree, 0, intermediateLabel);
+		String intermediateLabel = "@" + label + "->";
+		Tree<String> intermediateTree = binarizeTreeHelper(tree, 0, intermediateLabel);
 		return new Tree<String>(label, intermediateTree.getChildren());
 	}
 
-	private static Tree<String> binarizeTreeHelper(Tree<String> tree,
-			int numChildrenGenerated, 
+	private static Tree<String> binarizeTreeHelper(Tree<String> tree, int numChildrenGenerated,
 			String intermediateLabel) {
 		Tree<String> leftTree = tree.getChildren().get(numChildrenGenerated);
 		List<Tree<String>> children = new ArrayList<Tree<String>>();
 		children.add(binarizeTree(leftTree));
 		if (numChildrenGenerated < tree.getChildren().size() - 1) {
-			Tree<String> rightTree = 
-					binarizeTreeHelper(tree, numChildrenGenerated + 1, 
-							intermediateLabel + "_" + leftTree.getLabel());
+			Tree<String> rightTree = binarizeTreeHelper(tree, numChildrenGenerated + 1,
+					intermediateLabel + "_" + leftTree.getLabel());
 			children.add(rightTree);
 		}
 		return new Tree<String>(intermediateLabel, children);
-	} 
+	}
 
 	public static Tree<String> unAnnotateTree(Tree<String> annotatedTree) {
 
 		// Remove intermediate nodes (labels beginning with "@"
 		// Remove all material on node labels which follow their base symbol
 		// (cuts at the leftmost - or ^ character)
-		// Examples: a node with label @NP->DT_JJ will be spliced out, 
+		// Examples: a node with label @NP->DT_JJ will be spliced out,
 		// and a node with label NP^S will be reduced to NP
 
-		Tree<String> debinarizedTree =
-				Trees.spliceNodes(annotatedTree, new Filter<String>() {
-					public boolean accept(String s) {
-						return s.startsWith("@");
-					}
-				});
-		Tree<String> unAnnotatedTree = 
-				(new Trees.FunctionNodeStripper()).transformTree(debinarizedTree);
-    Tree<String> unMarkovizedTree =
-        (new Trees.MarkovizationAnnotationStripper()).transformTree(unAnnotatedTree);
+		Tree<String> debinarizedTree = Trees.spliceNodes(annotatedTree, new Filter<String>() {
+			public boolean accept(String s) {
+				return s.startsWith("@");
+			}
+		});
+		Tree<String> unAnnotatedTree = (new Trees.FunctionNodeStripper()).transformTree(debinarizedTree);
+		Tree<String> unMarkovizedTree = (new Trees.MarkovizationAnnotationStripper()).transformTree(unAnnotatedTree);
 		return unMarkovizedTree;
+	}
+
+	private static Tree<String> VerticalMarkovnize(Tree<String> tree, String parent, boolean withParent) {
+		if (tree.isLeaf()) {
+			return new Tree<String>(tree.getLabel());
+		}
+		String currentLabel = "";
+		if (parent.length() != 0) {
+			if (order == 1 || withParent)
+				currentLabel = tree.getLabel();
+			else if (order == 2) {
+				currentLabel = tree.getLabel() + "^" + "parent";
+				withParent = true;
+			}
+		}
+
+		ArrayList<Tree<String>> children = new ArrayList<Tree<String>>();
+		for (Tree<String> node : tree.getChildren()) {
+			children.add(VerticalMarkovnize(node, currentLabel, withParent));
+		}
+		return new Tree<String>(currentLabel, children);
 	}
 }
